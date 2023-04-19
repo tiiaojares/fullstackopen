98103f2c5d040b0b93ae9b41047e42343a7b46fe
tiia <tiia.ojares@gmail.com>
2023-04-19 14:12:19 +0300

    tehtävät 1.3-1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,69 @@
 
+// tehtävät 1.3-1.5:
+
 
 const Header = (props) => {
+  console.log(props);
+  return <h1> {props.name} </h1>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part ={props.parts[0]} />
+      <Part part ={props.parts[1]} />
+      <Part part ={props.parts[2]} />
+    </div>
+  )
+}
+const Total = (props) => {
+  let number = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
+  return <p>Number of exercises {number}</p>
+}
+
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+// tehtävät 1.1-1.2:
+/*
+
+const Header = (props) => {
+  console.log(props);
   return <h1> {props.course} </h1>
 }
 
@@ -44,6 +107,7 @@ const App = () => {
     </div>
   )
 }
+*/
 
 export default App
 
